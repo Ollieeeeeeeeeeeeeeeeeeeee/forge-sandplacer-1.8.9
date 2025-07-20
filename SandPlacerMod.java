73e5178a2f513fb027448ac738c236d7344a08a0
Ollieeeeeeeeeeeeeeeeeeeee <Olliemarchant27@Gmail.com>
2025-07-20 08:26:27 +0100

    Create SandPlacerMod.java

---
+++ SandPlacerMod.java
@@ -0,0 +1,18 @@
+package com.example.sandplacer;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid = SandPlacerMod.MODID, version = SandPlacerMod.VERSION)
+public class SandPlacerMod {
+    public static final String MODID = "sandplacer";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new SandTask());
+    }
+}
